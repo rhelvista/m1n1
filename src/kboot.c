014520153f4614daca9b7c9261236f3d14e7f30a
rhlvista <yesthatsmyemail321@gmail.com>
2026-07-18 02:15:33 -0500

    Fixes for the M5 to boot with reserved memory
    Signed-off-by: rhlvista <yesthatsmyemail321@gmail.com>

--- src/kboot.c
+++ src/kboot.c
@@ -359,7 +359,12 @@ static int dt_set_memory(void)
     // iOS 12 and below seems to be missing a bunch of stuff...
     if (ADT_GETPROP(adt, anode, "dram-base", &dram_base) < 0) {
         printf("ADT: Failed to get dram-base\n");
-        dram_base = 0x800000000;
+        if (fdt_node_offset_by_compatible(dt, -1, "apple,t8142-aic3")){
+            dram_base = 0x10000000000;
+        }else{
+            dram_base = 0x800000000;
+        }
+
     }
 
     if (ADT_GETPROP(adt, anode, "dram-size", &dram_size) < 0) {
@@ -2867,7 +2872,7 @@ int kboot_boot(void *kernel)
 
     usb_init();
     pcie_init();
-    dapf_init_all();
+    //dapf_init_all();
 
     printf("Setting SMP mode to WFE...\n");
     smp_set_wfe_mode(true);
